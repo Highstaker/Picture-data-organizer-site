8e6a0f827d092bd31d775e314c6d86faff4701af
Highstaker <heights999@yandex.ru>
2017-01-29 23:01:43 +0300

    v.0.1.0
    initial

---
+++ photodata_organizer.py
@@ -0,0 +1,98 @@
+from os import path, makedirs
+import json
+
+import dryscrape
+from bs4 import BeautifulSoup as bs
+import urllib.request
+from urllib.parse import urlparse
+from threading import Thread
+
+from flask import Flask, render_template
+
+VERSION = (0, 1, 0)
+
+SCRIPT_FOLDER = path.dirname(path.realpath(__file__))
+
+HOST_NAME = "127.0.0.1"
+HOST_PORT = 12365
+
+SOURCE_URL = "https://www.nordicfuzzcon.org/Registration/FursuitList"
+
+TEMP_IMAGES_FOLDER = path.join("/tmp", "img")
+
+APP_NAME = "fursuiters-organizer"
+
+application = Flask(__name__, static_url_path='', static_folder=path.join(SCRIPT_FOLDER, 'static'))
+
+
+def extract_data():
+	data = []
+	makedirs(TEMP_IMAGES_FOLDER, exist_ok=True)
+
+	session = dryscrape.Session()
+	session.visit(SOURCE_URL)
+	response = session.body()
+
+	soup = bs(response, "lxml")
+
+	entries = soup.find_all("div", class_="fursuitList__tile")
+
+	# print(entries)#debug
+
+	image_urls = list()
+	for entry in entries:
+		chunk = dict()
+		text_info = entry.find("div", class_='fursuitList__tile__details')
+		for br in text_info.find_all("br"):
+			br.replace_with("\n")
+		# print(text_info.text)#debug
+
+		# process text data
+		for line in text_info.text.split("\n"):
+			line_parse = line.split(":", 1)
+			try:
+				chunk[line_parse[0]] = line_parse[1].strip(" \n\r\t")
+			except KeyError:
+				pass
+
+		# store country data
+		country = entry.find("img", class_="fursuitList__tile__flag")["title"]
+		chunk["Country"] = country
+
+		# store respective image filenames
+		image_url = entry.find("img", class_="fursuitList__tile__imageActual")["src"]
+		image_urls.append(image_url)
+		image_filename = path.basename(urlparse(image_url).path)
+		chunk["image_filename"] = image_filename
+
+		data.append(chunk)
+
+	# Download images
+	threads = []
+	for image_url in image_urls:
+		image_filename = path.basename(urlparse(image_url).path)
+		t = Thread(target=urllib.request.urlretrieve,
+							args=(image_url, path.join(TEMP_IMAGES_FOLDER, image_filename)))
+		threads.append(t)
+		t.start()
+	for t in threads:
+		t.join()
+
+	return data
+
+
+@application.route('/')
+def index():
+	return render_template('index.html')
+
+
+@application.route('/get_data')
+def api_get_data():
+	data = json.dumps(SOURCE_DATA)
+	return data
+
+if __name__ == '__main__':
+	SOURCE_DATA = extract_data()
+	print(SOURCE_DATA)#debug
+	application.run(debug=True, host=HOST_NAME, port=HOST_PORT, threaded=True, use_reloader=False)
+	
---
+++ static/js/main.js
@@ -0,0 +1,82 @@
+$(function(){
+    console.log("ready!");
+
+    var source_data;
+    var pic_id_prefix = "img-"
+
+    var get_info = function(id){
+        //takes the id of a picture and returns full info in HTML for the info box.
+        index = parseInt(id.slice(pic_id_prefix.length));
+        result = "";
+        // console.log(index);
+
+        result += "<p>Character name: " + source_data[index]["Fursuit"] + "</p>";
+        result += "<p>Country: " + source_data[index]["Country"] + "</p>";
+        result += "<p>Species: " + source_data[index]["Species"] + "</p>";
+        result += "<p>Wearer: " + source_data[index]["Wearer"] + "</p>";
+
+        return result;
+    }
+
+    var collect_data = function(){
+        $.ajax({
+            url: "get_data",
+            type: "GET",
+            dataType: "Json",
+            success: function(data, status){
+                source_data = data;
+                show_photos();
+                $(".photo").hover(
+                    function(event){
+                        // console.log("entered " + this.id);//debug
+                        $("#info-box").css({"visibility": "visible"});
+                        $("#info-box").html(get_info(this.id));
+                        },//end enter function
+                    function(event){
+                        // console.log("leaving " + this.id);//debug
+                        $("#info-box").css({"visibility": "hidden"});
+                        /*end exit function*/});
+            }//success
+        });//ajax
+    };//collect_data
+
+    var show_photos = function(){
+        console.log(source_data);//debug
+        for(var i=0;i<source_data.length;i++)
+        {
+            var img = $('<img>', {
+                id: 'pic-'+i,
+                class: 'photo',
+                src: "img/"+source_data[i]["image_filename"]
+            });
+
+            img_container = $('<div>', {
+                class: 'photo-container',
+                id: 'pic-'+i+'-container',
+            });
+            img.appendTo(img_container);
+            img_container.appendTo('#photo-screen');
+        }
+    };//show_photos
+
+    collect_data();//run ajax
+
+    $("#photo-screen").mousemove(function(event){
+    	var mouseX = event.pageX;
+    	var mouseY = event.pageY;
+    	var window_width = $(window).width()
+    	var middle = window_width / 2;
+    	// console.log(mouseX + " " + mouseY + " " + window_width + " " + middle);//debug
+
+    	if(mouseX < middle)
+	    	{
+	    		$("#info-box").css({"right": "0"});
+	    	}
+	    	else
+	    	{
+	    		$("#info-box").css({"right": "auto"});
+	    	}
+    });//$("#photo-screen").mousemove
+
+
+    });//$
