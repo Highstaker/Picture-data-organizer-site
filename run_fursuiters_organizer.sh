8614d17cbc28f259344208800696cb198f5d3907
Highstaker <heights999@yandex.ru>
2017-02-01 13:42:06 +0300

    v.0.2.2r1
    +a symlink to interpreter is now created, that names the python process accordingly, so I could find it easily when it it running

--- run_fursuiters_organizer.sh
+++ run_fursuiters_organizer.sh
@@ -5,6 +5,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR
 
-env/bin/python3 photodata_organizer.py
+PYTHON_INTERPRETER_LINK_NAME="python_NFC_fursuiters_organizer"
+# create link
+ln -s env/bin/python3 $SCRIPT_DIR/$PYTHON_INTERPRETER_LINK_NAME
+
+# run the server
+$SCRIPT_DIR/$PYTHON_INTERPRETER_LINK_NAME photodata_organizer.py
 
 exit 0;
